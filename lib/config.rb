08276a1f891f61668de41721f3d76115da89ee3f
O. Bucao <orlando.bucao@biola.edu>
2016-06-28 10:18:31 -0700

    Refactor Pawn to add orientation. Refactor Config.

--- lib/config.rb
+++ lib/config.rb
@@ -1,16 +1,14 @@
-class Config
+module Config
   SETTINGS = {
-    1 =>
-    { black: [[Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook],
-              [Pawn, Pawn,   Pawn,   Pawn,  Pawn, Pawn,   Pawn,   Pawn]],
-      white: [[Pawn, Pawn,   Pawn,   Pawn,  Pawn, Pawn,   Pawn,   Pawn],
-              [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]]
-     },
-     2 =>
-    { white: [[Rook, Knight, Bishop, King, Queen, Bishop, Knight, Rook],
-              [Pawn, Pawn,   Pawn,   Pawn,  Pawn, Pawn,   Pawn,   Pawn]],
-      black: [[Pawn, Pawn,   Pawn,   Pawn,  Pawn, Pawn,   Pawn,   Pawn],
-              [Rook, Knight, Bishop, King, Queen, Bishop, Knight, Rook]]
-     }
+    1 => { black: [[Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook],
+                   [Pawn, Pawn,   Pawn,   Pawn,  Pawn, Pawn,   Pawn,   Pawn]],
+           white: [[Pawn, Pawn,   Pawn,   Pawn,  Pawn, Pawn,   Pawn,   Pawn],
+                   [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]]
+          },
+    2 => { white: [[Rook, Knight, Bishop, King, Queen, Bishop, Knight, Rook],
+                   [Pawn, Pawn,   Pawn,   Pawn,  Pawn, Pawn,   Pawn,   Pawn]],
+           black: [[Pawn, Pawn,   Pawn,   Pawn,  Pawn, Pawn,   Pawn,   Pawn],
+                   [Rook, Knight, Bishop, King, Queen, Bishop, Knight, Rook]]
+          }
   }
 end
--- lib/pawn.rb
+++ lib/pawn.rb
@@ -3,30 +3,30 @@ require_relative 'piece'
 class Pawn < Piece
 
   AVATARS = { white: "\u2659", black: "\u265F"}
-  
-  MOVE_MAPPINGS = { top: { down_verticals: DOWN_VERTICALS },
-                    bottom: { up_verticals: UP_VERTICALS } }
 
-  SPECIAL_MAPPINGS = { top: { down_left_diagonals: DOWN_LEFT_DIAGONALS, 
+  MOVE_MAPPINGS = { facing_down: { down_verticals: DOWN_VERTICALS },
+                    facing_up: { up_verticals: UP_VERTICALS } }
+
+  SPECIAL_MAPPINGS = { facing_down: { down_left_diagonals: DOWN_LEFT_DIAGONALS,
                                 down_right_diagonals: DOWN_RIGHT_DIAGONALS },
-                       bottom: { up_left_diagonals: UP_LEFT_DIAGONALS, 
+                       facing_up: { up_left_diagonals: UP_LEFT_DIAGONALS,
                                 up_right_diagonals: UP_RIGHT_DIAGONALS } }
 
   def initialize(board:, color:, start_pos:)
     super
-    position = start_pos[1] <= 3 ? :top : :bottom
-    self.class.define_movement_methods(MOVE_MAPPINGS[position])
-    self.class.define_movement_methods(SPECIAL_MAPPINGS[position])
+    @orientation = start_pos[1] <= 3 ? :facing_down : :facing_up
+    self.class.define_movement_methods(MOVE_MAPPINGS[@orientation])
+    self.class.define_movement_methods(SPECIAL_MAPPINGS[@orientation])
   end
 
   def available_moves
-    moves = get_available_moves(mappings: MOVE_MAPPINGS[color], levels: 2) do |memo|
+    moves = get_available_moves(mappings: MOVE_MAPPINGS[@orientation], levels: 2) do |memo|
       toggle_first_move(memo)
     end
   end
 
   def special_moves
-    moves = get_available_moves(mappings: SPECIAL_MAPPINGS[color], levels: 1) do |memo|
+    moves = get_available_moves(mappings: SPECIAL_MAPPINGS[@orientation], levels: 1) do |memo|
       remove_unoccupied(memo)
     end
   end
