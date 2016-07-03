b0cca79dc05a1531751bd5def6c4298e003dcde7
O. Bucao <ojbucao@gmail.com>
2016-07-03 08:34:53 -0700

    Add #enpassantables

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -12,11 +12,15 @@ class Pawn < Piece
                        facing_up: { up_left_diagonals: Directable::UP_LEFT_DIAGONALS,
                                     up_right_diagonals: Directable::UP_RIGHT_DIAGONALS } }
 
+  SIDE_MAPPINGS =  { left_side: Directable::LEFT_HORIZONTALS, 
+                     right_side: Directable::RIGHT_HORIZONTALS }
+
   def initialize(board:, color:, start_pos:)
     super
     @orientation = start_pos[1] <= 3 ? :facing_down : :facing_up
     self.class.define_movement_methods(MOVE_MAPPINGS[@orientation])
     self.class.define_movement_methods(SPECIAL_MAPPINGS[@orientation])
+    self.class.define_movement_methods(SIDE_MAPPINGS)
   end
 
   def available_moves
@@ -41,6 +45,20 @@ class Pawn < Piece
     locations = reorient_locations(moves)
     locations.pop if move_count > 0 && locations.count > 1
     locations
+  end
+
+  def enpassantables
+    targets = get_available_moves(mappings: SPECIAL_MAPPINGS[@orientation], levels: 1)
+
+    enpassant = targets.inject({}) do |memo, target|
+      side = [target[0],current_location[1]]
+      if side == @board.last_location_used && @board.piece_at(side).move_count == 1
+        memo[target] = @board.piece_at(side)
+      end
+      memo
+    end
+    return nil if enpassant.empty?
+    enpassant
   end
 
 end
